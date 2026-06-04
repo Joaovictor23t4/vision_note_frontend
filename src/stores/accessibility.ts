c104f48f06dbc7a66d077ae955e08d490c28f6d4
Joaovictor23t4 <joaovictor239090@gmail.com>
2026-06-04 19:40:46 -0300

    FEAT: Implement change font size dynamically #7

--- src/stores/accessibility.ts
+++ src/stores/accessibility.ts
@@ -4,12 +4,12 @@ import { reactive } from "vue";
 export const useAccessibilityStore = defineStore('accessibility', () => {
     interface StateInterface {
         theme: string,
-        font_size: string
+        font_size: number
     };
 
     const state = reactive<StateInterface>({
         theme: localStorage.getItem("data-theme") ?? "high_contrast",
-        font_size: localStorage.getItem("data-font_size") ?? "18"
+        font_size: Number(localStorage.getItem("data-font-size")) ?? 18
     });
 
     function setTheme(theme: string): void {
@@ -18,10 +18,10 @@ export const useAccessibilityStore = defineStore('accessibility', () => {
         localStorage.setItem("data-theme", theme);
     }
 
-    function setFontSize(fontSize: string): void {
+    function setFontSize(fontSize: number): void {
         state.font_size = fontSize;
-        document.documentElement.setAttribute("data-font-s", fontSize);
-        localStorage.setItem("data-font_size", fontSize);
+        document.documentElement.setAttribute("data-font-s", fontSize.toString());
+        localStorage.setItem("data-font-size", fontSize.toString());
     }
 
     return {
